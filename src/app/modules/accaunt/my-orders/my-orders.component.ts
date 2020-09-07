91b0b92ed1acace45664607afc54d64307c43176
volodiabukovskyi186 <volodiabykovskii@gmail.com>
2020-09-07 16:09:15 +0300

    first commit

--- src/app/modules/accaunt/my-orders/my-orders.component.ts
+++ src/app/modules/accaunt/my-orders/my-orders.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-my-orders',
-  templateUrl: './my-orders.component.html',
-  styleUrls: ['./my-orders.component.scss']
+    selector: 'app-my-orders',
+    templateUrl: './my-orders.component.html',
+    styleUrls: ['./my-orders.component.scss']
 })
 export class MyOrdersComponent implements OnInit {
+    itemStatus = false;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+
+    itemDrop(): void {
+        this.itemStatus == false ? this.itemStatus = true : this.itemStatus = false;
+        console.log(this.itemStatus);
+    }
 
 }
--- src/app/modules/accaunt/personal-data/personal-data.component.ts
+++ src/app/modules/accaunt/personal-data/personal-data.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AccauntService} from '../accaunt.service';
 
 @Component({
-  selector: 'app-personal-data',
-  templateUrl: './personal-data.component.html',
-  styleUrls: ['./personal-data.component.scss']
+    selector: 'app-personal-data',
+    templateUrl: './personal-data.component.html',
+    styleUrls: ['./personal-data.component.scss']
 })
 export class PersonalDataComponent implements OnInit {
+    personStatus = false;
 
-  constructor(public accaunt: AccauntService) { }
+    constructor(public accaunt: AccauntService) {
+    }
 
-  ngOnInit(): void {
-    this.accaunt.getUser().subscribe((data) => {
-      this.accaunt.current = data.data;
-      this.accaunt.onCurrent();
-    });
-  }
+    ngOnInit(): void {
+        this.accaunt.getUser().subscribe((data) => {
+            this.accaunt.current = data.data;
+            this.accaunt.onCurrent();
+        });
+    }
+
+    changeStatus(): void {
+        this.personStatus == false ? this.personStatus = true : this.personStatus = false;
+    }
 
 }
